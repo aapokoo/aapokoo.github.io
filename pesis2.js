a2e74ad636ab0b11d1f121d2833294223d95b0f2
aapokoo <33426389+aapokoo@users.noreply.github.com>
2022-08-20 14:34:50 +0300

    add js file

---
+++ pesis2.js
@@ -0,0 +1,483 @@
+var maat = ["pata", "ruutu", "risti", "hertta"];
+var arvot = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let pakka = new Array();
+let kortti;
+let juoksut = 0;
+let jokeri;
+let batter = 0;
+let vierasjoukkue;
+let valittuMaa;
+let valittuMaa2;
+let grafiikkaArvo;
+
+function getRandomInt(max) { //random numero väliltä 0 - syötetty arvo
+    return Math.floor(Math.random() * max);
+  }
+
+function vastustaja() {
+    document.getElementById("vastustaja").style.display = "none";
+    vierasjoukkue = getRandomInt(2);
+    if (vierasjoukkue === 0) {
+        document.getElementById("vierastulos").innerHTML = "3";
+        document.getElementById("vieraslogo").src="/users/Aapi/Documents/Pesis/Pesis2/kuvat2/JymyJussit-STAMP.png";
+        vierastulos = 3;
+    } else {
+        document.getElementById("vierastulos").innerHTML = "7";
+        document.getElementById("vieraslogo").src="/users/Aapi/Documents/Pesis/Pesis2/kuvat2/sojy_merkki_w.png";
+        vierastulos = 7;
+    }
+}
+
+function rakennaPakka() //rakentaa pakan
+{
+    document.getElementById("pakanRakennus").style.display = "none";
+	for(let i = 0; i < maat.length; i++)
+	{
+		for(let x = 0; x < arvot.length; x++)
+		{
+			kortti = {Arvo: arvot[x], Maa: maat[i]};
+			pakka.push(kortti);  
+		}
+	}
+    for (let j = 0; j < 3; j++)
+    {
+        jokeri = {Arvo: "Joker", Maa: "Joker"};
+        pakka.push(jokeri);
+    }
+	return pakka;
+}
+
+function sekoita(pakka) //sekoittaa pakan
+{
+    document.getElementById("pakanSekoitus").style.display = "none";
+	// for 1000 turns
+	// switch the values of two random cards
+	for (let i = 0; i < 1000; i++)
+	{
+		let location1 = Math.floor((Math.random() * pakka.length));
+		let location2 = Math.floor((Math.random() * pakka.length));
+		let tmp = pakka[location1];
+
+		pakka[location1] = pakka[location2];
+		pakka[location2] = tmp;
+	}
+}
+
+function nostaKortti(pakka) //nostaa päällimmäisen kortin
+{
+    return pakka.pop();
+}
+
+function nostettuKortti(kortti) { //kortti grafiikat
+    let grafiikkaArvo = kortti.Arvo;
+    if (grafiikkaArvo === "1") {
+        grafiikkaArvo = "A";
+    }
+    if (kortti.Maa === "hertta") {
+        document.getElementById("nosto").innerHTML = "♥" + " " + grafiikkaArvo;
+        document.getElementById("nosto").style.color = 'red';
+        document.getElementById("nosto").style.fontSize = "48px";
+    } else if (kortti.Maa === "ruutu") {
+        document.getElementById("nosto").innerHTML = "♦" + " " + grafiikkaArvo;
+        document.getElementById("nosto").style.color = 'red';
+        document.getElementById("nosto").style.fontSize = "48px";
+    } else if (kortti.Maa === "pata") {
+        document.getElementById("nosto").innerHTML = "♠" + " " + grafiikkaArvo;
+        document.getElementById("nosto").style.color = 'black';
+        document.getElementById("nosto").style.fontSize = "48px";
+    } else if (kortti.Maa === "risti") {
+        document.getElementById("nosto").innerHTML = "♣" + " " + grafiikkaArvo;
+        document.getElementById("nosto").style.color = 'black';
+        document.getElementById("nosto").style.fontSize = "48px";
+    } else {
+        document.getElementById("nosto").innerHTML = "*Joker*";
+        document.getElementById("nosto").style.color = 'black';
+        document.getElementById("nosto").style.fontSize = "30px";
+    }
+}
+
+function maaHertta() {
+    valittuMaa = "hertta";
+    valittuMaa2 = "ruutu";
+    document.getElementById("herttaValinta").style.display = "none";
+    document.getElementById("ruutuValinta").style.display = "none";
+    document.getElementById("ristiValinta").style.display = "none";
+    document.getElementById("pataValinta").style.display = "none";
+    document.getElementById("maanValinta").style.display = "none";
+}
+function maaRuutu() {
+    valittuMaa = "ruutu";
+    valittuMaa2 = "hertta";
+    document.getElementById("herttaValinta").style.display = "none";
+    document.getElementById("ruutuValinta").style.display = "none";
+    document.getElementById("ristiValinta").style.display = "none";
+    document.getElementById("pataValinta").style.display = "none";
+    document.getElementById("maanValinta").style.display = "none";
+}
+function maaRisti() {
+    valittuMaa = "risti";
+    valittuMaa2 = "pata";
+    document.getElementById("herttaValinta").style.display = "none";
+    document.getElementById("ruutuValinta").style.display = "none";
+    document.getElementById("ristiValinta").style.display = "none";
+    document.getElementById("pataValinta").style.display = "none";
+    document.getElementById("maanValinta").style.display = "none";
+}
+function maaPata() {
+    valittuMaa = "pata";
+    valittuMaa2 = "risti";
+    document.getElementById("herttaValinta").style.display = "none";
+    document.getElementById("ruutuValinta").style.display = "none";
+    document.getElementById("ristiValinta").style.display = "none";
+    document.getElementById("pataValinta").style.display = "none";
+    document.getElementById("maanValinta").style.display = "none";
+}
+
+let joukkue = { //joukkueen pelaajat ja statsit
+    pelaajat: {
+        lukkari: {
+            nimi: 'Kinnunen',
+            lyöMin: 2,
+            lyöMax: 9,
+            lyöKahdesti: false,
+            lyöJokereita: false,
+        },
+        sieppari: {
+            nimi: 'Rinta-aho',
+            lyöMin: 1,
+            lyöMax: 10,
+            lyöKahdesti: false,
+            lyöJokereita: false,
+        },
+        ykkösvahti: {
+            nimi: 'A. Lassila',
+            lyöMin: 1,
+            lyöMax: 10,
+            lyöKahdesti: false,
+            lyöJokereita: false,
+        },
+        kakkosvahti: {
+            nimi: 'Saukko',
+            lyöMin: 2,
+            lyöMax: 10,
+            lyöKahdesti: false,
+            lyöJokereita: false,
+        },
+        kakkospolttaja: {
+            nimi: 'S. Lassila',
+            lyöMin: 1,
+            lyöMax: 10,
+            lyöKahdesti: false,
+            lyöJokereita: false,
+        },
+        kolmospolttaja: {
+            nimi: 'Heikkala',
+            lyöMin: 2,
+            lyöMax: 9,
+            lyöKahdesti: false,
+            lyöJokereita: false,
+        },
+        kolmosvahti: {
+            nimi: 'L. Vihriälä',
+            lyöMin: 2,
+            lyöMax: 10,
+            lyöKahdesti: false,
+            lyöJokereita: false,
+        },
+        kakkoskoppari: {
+            nimi: 'Anttila',
+            lyöMin: 1,
+            lyöMax: 10,
+            lyöKahdesti: false,
+            lyöJokereita: false,
+        },
+        kolmoskoppari: {
+            nimi: 'M. Vihriälä',
+            lyöMin: 2,
+            lyöMax: 10,
+            lyöKahdesti: false,
+            lyöJokereita: false,
+        },
+        jokeri: {
+            nimi: 'Vainionpää',
+            lyöMin: 1,
+            lyöMax: 10,
+            lyöKahdesti: true,
+            lyöJokereita: true,
+        }
+    } 
+}
+
+function lyö1() {
+    kortti = nostaKortti(pakka);
+    nostettuKortti(kortti);
+   
+    if (kortti.Maa === valittuMaa && kortti.Arvo >= 2 && kortti.Arvo <= 9) {
+        juoksut++;
+        document.getElementById("kotitulos").innerHTML = juoksut;
+    
+    } else {
+        function hide1() {
+        document.getElementById("pakka1").style.display = "none";
+        }
+        hide1();
+        document.getElementById("0").style.backgroundColor = "gray";
+        function show1() {
+            document.getElementById("pakka2").style.visibility = "visible";
+        }
+        show1();
+    }
+}
+
+function lyö2() {
+    kortti = nostaKortti(pakka);
+    nostettuKortti(kortti);
+   
+    if (kortti.Maa === valittuMaa && kortti.Arvo >= 1 && kortti.Arvo <= 10) {
+        juoksut++;
+        document.getElementById("kotitulos").innerHTML = juoksut;
+    } else if (kortti.Maa === valittuMaa2 && kortti.Arvo >= 1 && kortti.Arvo <= 10) {
+        juoksut++;
+        document.getElementById("kotitulos").innerHTML = juoksut;
+    } else {
+        function hide2() {
+        document.getElementById("pakka2").style.display = "none";
+        }
+        hide2();
+        document.getElementById("1").style.backgroundColor = "gray";
+        function show2() {
+            document.getElementById("pakka3").style.visibility = "visible";
+        }
+        show2();
+    }
+}
+
+function lyö3() {
+    kortti = nostaKortti(pakka);
+    nostettuKortti(kortti);
+   
+    if (kortti.Maa === valittuMaa && kortti.Arvo >= 1 && kortti.Arvo <= 10) {
+        juoksut++;
+        document.getElementById("kotitulos").innerHTML = juoksut;
+    
+    } else {
+        function hide3() {
+        document.getElementById("pakka3").style.display = "none";
+        }
+        hide3();
+        document.getElementById("2").style.backgroundColor = "gray";
+        function show3() {
+            document.getElementById("pakka4").style.visibility = "visible";
+        }
+        show3();
+    }
+}
+
+function lyö4() {
+    kortti = nostaKortti(pakka);
+    nostettuKortti(kortti);
+   
+    if (kortti.Maa === valittuMaa && kortti.Arvo >= 2 && kortti.Arvo <= 10) {
+        juoksut++;
+        document.getElementById("kotitulos").innerHTML = juoksut;
+    
+    } else {
+        function hide4() {
+        document.getElementById("pakka4").style.display = "none";
+        }
+        hide4();
+        document.getElementById("3").style.backgroundColor = "gray";
+        function show4() {
+            document.getElementById("pakka5").style.visibility = "visible";
+        }
+        show4();
+    }
+}
+
+function lyö5() {
+    kortti = nostaKortti(pakka);
+    nostettuKortti(kortti);
+   
+    if (kortti.Maa === valittuMaa && kortti.Arvo >= 1 && kortti.Arvo <= 10) {
+        juoksut++;
+        document.getElementById("kotitulos").innerHTML = juoksut;
+    
+    } else {
+        function hide5() {
+        document.getElementById("pakka5").style.display = "none";
+        }
+        hide5();
+        document.getElementById("4").style.backgroundColor = "gray";
+        function show5() {
+            document.getElementById("pakka6").style.visibility = "visible";
+        }
+        show5();
+    }
+}
+
+function lyö6() {
+    kortti = nostaKortti(pakka);
+    nostettuKortti(kortti);
+   
+    if (kortti.Maa === valittuMaa && kortti.Arvo >= 2 && kortti.Arvo <= 9) {
+        juoksut++;
+        document.getElementById("kotitulos").innerHTML = juoksut;
+    
+    } else {
+        function hide6() {
+        document.getElementById("pakka6").style.display = "none";
+        }
+        hide6();
+        document.getElementById("5").style.backgroundColor = "gray";
+        function show6() {
+            document.getElementById("pakka7").style.visibility = "visible";
+        }
+        show6();
+    }
+}
+
+function lyö7() {
+    kortti = nostaKortti(pakka);
+    nostettuKortti(kortti);
+   
+    if (kortti.Maa === valittuMaa && kortti.Arvo >= 2 && kortti.Arvo <= 10) {
+        juoksut++;
+        document.getElementById("kotitulos").innerHTML = juoksut;
+    
+    } else {
+        function hide7() {
+        document.getElementById("pakka7").style.display = "none";
+        }
+        hide7();
+        document.getElementById("6").style.backgroundColor = "gray";
+        function show7() {
+            document.getElementById("pakka8").style.visibility = "visible";
+        }
+        show7();
+    }
+}
+
+function lyö8() {
+    kortti = nostaKortti(pakka);
+    nostettuKortti(kortti);
+   
+    if (kortti.Maa === valittuMaa && kortti.Arvo >= 1 && kortti.Arvo <= 10) {
+        juoksut++;
+        document.getElementById("kotitulos").innerHTML = juoksut;
+    
+    } else if (kortti.Maa === valittuMaa2 && kortti.Arvo >= 1 && kortti.Arvo <= 10) {
+        juoksut++;
+        document.getElementById("kotitulos").innerHTML = juoksut;
+    } else {
+        function hide8() {
+        document.getElementById("pakka8").style.display = "none";
+        }
+        hide8();
+        document.getElementById("7").style.backgroundColor = "gray";
+        function show8() {
+            document.getElementById("pakka9").style.visibility = "visible";
+        }
+        show8();
+    }
+}
+
+function lyö9() {
+    kortti = nostaKortti(pakka);
+    nostettuKortti(kortti);
+   
+    if (kortti.Maa === valittuMaa && kortti.Arvo >= 2 && kortti.Arvo <= 10) {
+        juoksut++;
+        document.getElementById("kotitulos").innerHTML = juoksut;
+    
+    } else {
+        function hide9() {
+        document.getElementById("pakka9").style.display = "none";
+        }
+        hide9();
+        document.getElementById("8").style.backgroundColor = "gray";
+        function show9() {
+            document.getElementById("pakka10").style.visibility = "visible";
+        }
+        show9();
+    }
+}
+
+function lyö10() {
+    kortti = nostaKortti(pakka);
+    nostettuKortti(kortti);
+    if (kortti.Maa === "Joker") {
+        juoksut++;
+        document.getElementById("kotitulos").innerHTML = juoksut;
+    }
+    if (kortti.Maa === valittuMaa && kortti.Arvo >= 1 && kortti.Arvo <= 10) {
+        juoksut++;
+        document.getElementById("kotitulos").innerHTML = juoksut;
+    } else if (kortti.Maa === valittuMaa2 && kortti.Arvo >= 1 && kortti.Arvo <= 10) {
+        juoksut++;
+        document.getElementById("kotitulos").innerHTML = juoksut;
+    } else {
+        function hide10() {
+        document.getElementById("pakka10").style.display = "none";
+        }
+        hide10();
+        document.getElementById("9").style.backgroundColor = "gray";
+        function show10() {
+            document.getElementById("pakka11").style.visibility = "visible";
+        }
+        show10();
+        if (juoksut > vierastulos) {
+            document.getElementById("lopputulos").innerHTML = "Voitto!";
+        } else if (juoksut === vierastulos) {
+            document.getElementById("lopputulos").innerHTML = "Tasapeli!";
+        } else {
+            document.getElementById("lopputulos").innerHTML = "Tappio!";
+        }
+    }
+}
+
+//rakennaPakka(); //pakan rakennus
+//sekoita(pakka); //pakan sekoitus
+
+/*
+2-10, 2x, A		1-21
+2-10, A			22-42
+2-10			43-63
+2-9			    64-84
+2-8			    85-105
+2-7			    106-126
+2-6			    127-147
+2-5			    148-168
+2-4			    169-189
+2-3			    190-210
+
+
+1. Vainionpää
+7. Mäkelä
+26. Rinta-aho
+28. Anttila
+51. A Lassila
+52. S Lassila
+68. L Vihriälä
+77. Saukko
+78. M Vihriälä
+85. Kinnunen
+98. J Heikkala
+105. O Heikkala
+126. Kanala
+166. E Lassila
+172. V Takala
+
+Vimpeli		8
+Manse		8
+SoJy		7
+KPL		    7
+Tahko		6
+JoMa		6
+Kempele		5
+KiPa		4
+SJJ		    3
+IPV		    3
+PattU		3
+Kossu		2
+Kankaanpää	1
+*/
