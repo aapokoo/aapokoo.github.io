b49d15f11b05652f22ce09e0321e4e67f699fc8c
aapokoo <33426389+aapokoo@users.noreply.github.com>
2022-09-13 09:38:52 +0300

    Update pesis2.js

--- pesis2.js
+++ pesis2.js
@@ -2175,7 +2175,6 @@ function uusiPeli() {
         document.getElementById("uusiPeli").style.display = "none";
 	document.getElementById("pakka11").style.display = "none";
 		document.getElementById("vastustaja").style.display = "inline";
-		document.getElementById("pakanSekoitus").style.display = "inline";
 		document.getElementById("herttaValinta").style.display = "inline";
     		document.getElementById("ruutuValinta").style.display = "inline";
     		document.getElementById("ristiValinta").style.display = "inline";
