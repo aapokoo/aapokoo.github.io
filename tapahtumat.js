5a347d6ea79b8ca5626ab1371bf9504e6b5fd22c
aapokoo <33426389+aapokoo@users.noreply.github.com>
2022-09-26 11:58:01 +0300

    Update tapahtumat.js

--- tapahtumat.js
+++ tapahtumat.js
@@ -1,1 +1,158 @@
+/*
 
+PALLO POLTTAA. Vastustajalle juoksu? (ao-mies torjuu) (up-nopat)
+
+Sijat 1.-5. maksa palkintomatka 5.000
+Sijat 6.-10. -
+SIjat 11.-15. nosta bingotuloja 5.000
+(koskee kaikkia)
+EI PUDOTUSPELEISSÄ
+
+VASTUSTAJAN LUKKARI YRITTÄÄ KÄRPÄSTÄ KOLMOSELTA!
+Vain ao-mies pystyy lyömään pahasta väärästä joukkueellesi 1 juoksun.
+
+PELISI KULKEE. Seuraava mies lyö väriä, A!
+
+Jos tämä on kotiottelu, saat juoksun.
+
+Yritä nopilla 3 juoksun arvoista läpilyöntiä!
+
+Jos olet sarjataulukossa sijoilla 12-15., nosta kotikuntasi yritysten tuki pelaajahankintoja varten 10.000 mk.
+
+Sinulla olisi nyt tilaisuus ostaa tähtipelaaja (20.-26.000) hintaan 15.000 mk! Päätös on tehtävä heti.
+
+VASTUSTAJA PAINOSTAA. Ao-mies torjuu, muille 1 juoksun arvoinen lipsahdus. (ei kotona)
+
+TAKAA LÄPI! Kunnari tyhjään kenttään - sinulle 1 juoksu.
+
+Saat vapaataivaljuoksun.
+
+HERMOILUA! Vastustajalle 1 juoksu lisää? (up-nopat)
+
+Jos olet sijoilla 11.-15., saat juoksun ja 5.000 mk.
+
+HYVÄ LÄPILYÖNTIYRITYS. Vedä pakan välistä kortti. 
+JOKER = saat 4 juoksua
+A = saat 3 juoksua
+Q, K = saat 2 juoksua
+10, J = saat 1 juoksun
+2-9 = ei juoksua
+
+Lyö vielä kerran!
+
+PALLO POLTTAA HYPPYSISSÄ! Vastustaja ottaa 1 juoksun? (up-nopat)
+
+ILKEÄ KIERRE POLTTOLINJALLE... Vastustajalle juoksu? (up-nopat)
+
+Seuraava kotiottelusi on osa kotikuntasi vuosipäiväjuhallisuuksia. Saat kaksinkertaiset lipputulot. (ota talteen siihen asti)
+
+Tämä pelaaja loukkaantuu ja joutuu olemaan seuraavan ottelun sivussa. (Pelaat siis vajaalla miehistöllä!) LAITA KORTTI PELILAUDALLE KO. NRON KOHDALLE.
+
+Tämä mies hiiltyy ja ottaa E-varoituksen. (2 varoituksesta jouluu pelikieltoon loppukaudeksi.) Merkkaa E miehen kohdalle kirjanpitoon.
+
+LUKKARISI HERMOILEE. Vastustaja saa vapaataivaljuoksun. (kotona lukkari heittää nopat?)
+
+KIRI, KIRI! Jos olet häviöllä, saat juoksun.
+
+HERMOILUA! Vastustajalle juoksu? (ao-mies torjuu) (up-nopat)
+
+Tämä pelaaja saa E-varoituksen. (2 varoituksesta joutuu pelikieltoon loppukaudeksi.) Merkkaa E pelaajan kirjanpitoon.
+
+Kokeile vielä uusi kerta!
+
+Kotikuntasi kuljetusyhtiö tarjoutuu maksamaan seuraavan vierasottelusi matkakustannukset! (ota talteen siihen asti)
+
+Annapa jokerisi lyödä kerran tässä välissä.
+
+Maksa kentän kunnossapitotöistä 6.000 mk. (Ei koske sijoja 11.-15.)
+
+Jos tämä mies on ao-mies tai jokeri, saat 2 juoksua!
+
+MERKKI PÄÄLLÄ... Saat juoksun? (sp-nopat)
+
+Tästä eteenpäin KOKO JOUKKUEESI lyö ÄSSIÄ  ja ässät ovat 2 juoksua jokainen sekä JOKEREITA, jotka ovat 3 juoksua! (edelleen tietysti myös valitsemaasi maata) (koskee VAIN tätä ottelua!)
+
+Heitä tälle miehelle sekä sisä- että ulkopelinopat. (Sekä sinä että vastustaja voitte saada juoksun.)
+?
+
+LUKKARISI KÄY KUUMANA. Vastustajalle vapaataivaljuoksu?
+? (lukkarin up-arvot)
+
+KOMEA KUMURI TAKAKENTÄLLE. Saat juoksun?
+? (sp-nopat)
+
+Joukkettasi yritetään lahjoa! Jos suostut 0-häviöön SEURAAVASSA OTTELUSSA, saat 20.000 mk. Häpeällistä, mutta... niin, mitä teet? (ota kortti talteen ja tee päätöksesi tämän ottelun päätyttyä)
+
+MERKKI PÄÄLLÄ! Yrität peruslyöntiä väliin - 1 juoksu? (ao-mies onnistuu)
+? (sp-nopat)
+
+Kokeile vielä kerran!
+
+HUONO PALAUTUSHEITTO. Vastustaja ehtii nipin napin ja saa juoksun. (ei kotona)
+
+VIERASOTTELU: Vastustajalle jouksu? 
+? (up-nopat)
+KOTIOTTELU: Ao-mies tai jokeri lyö sinulle 1 juoksun, muut heittävät noppaa.
+? (sp-nopat)
+
+ONNETON YLIHEITTO. Vastustaja tekee 1 juoksun ja yrittää toista.
+(Toisen juoksun osalta? Up-nopat)
+
+Jos olet sijoilla 10.-15., päätät kokeilla salaista asetta: vanha tähtipelaaja Gunnar Laaka pelaa 2 SEURAAVAA OTTELUA joukkueessasi. Gunnar lyö VÄRIÄ 2X  myös A JA JOKER kelpaavat. SP- ja UP-arvot molemmat 3-18. Ota kortti talteen ja muista: Gunnarin kunto kestää vain 2 ottelua. (takakenttä tai polttolinja.) OTA TALTEEN.
+
+PAINOSTAT KOVASTI. Saat juoksun?
+? (sp-nopat)
+(jokeri ja ao-mies lyövät juoksun)
+
+Jos tämä lyöjä sattuu olemaan jokeri, saat juoksun.
+
+Jos tämä on kotiottelu, saat harhaheittojuoksun.
+
+KARKEA YLIHEITTO! Saat harhaheittojuoksun.
+
+Lyö 1.000 mk vetoa ottelun voittajasta. Jos veikkaat oikein, saat 5.000 mk. (Pidä kortti ottelun päättymiseen asti.)
+
+Maksa pelivälineistä 3.000 mk.
+
+VARMISTUS MYÖHÄSSÄ. Vastustajalle juoksu?
+? (up-nopat)
+
+KOTIOTTELU: Saat 1 juoksun.
+VIERASOTTELU: Vastustajalle juoksu?
+? (up-nopat) (ao-mies torjuu)
+
+Jos tämä on kotiottelu, anna PARHAAN (ts. saat valita) lyöjäsi yrittää KERRAN.
+
+Hengittääkö putoamispeikko joukkueesi niskaan= Jos olet sijoilla 9.-15-, saat juoksun.
+
+ONNETON HARHAHEITTO. Vastustajalle juoksu?
+? (up-nopat)
+
+PAHA NUOLIAINEN. Vastustajalle juoksu. (ei kotona) (ao-mies torjuu)
+
+Yritä juoksua hidastetulla
+? (sp-nopat) (ao-miehen ei tarvitse heittää noppia)
+
+NÄPÄYKSELLÄ JUOKSU? Saat yhden juoksun?
+? (sp-nopat) (kotona ei tarvita noppia)
+
+Tästä pelaajasta tarjotaan sinulle nyt hänestä maksamasi hinta + 5.000 mk. Siirto tulisi voimaan heti.
+
+TUNAROINTIA. Vastustajalle juoksu. (ao-mies ja jokeri lyövät vastaukseksi sinulle 2 juoksua)
+
+LOISTAVA RAJALEIKKAUS! Saat 2 juoksua läpilyönnillä.
+
+Jos olet sijoilla 11.-15., saat nimettömältä lahjoittajalta 10.000 mk.
+
+Tämän pelaajan kanssa on riitaa palkka-asioissa. Maksa hänelle lisää heti 4.000 mk tai joudut myymään hänet.
+
+PELOTTAVA LAAKA PESÄN TAAKSE. Vastustajalle juoksu?
+? (up-nopat)
+
+Lisenssimaksuissa sekaannusta. Maksa 4.000 mk.
+
+KAUNIS VÄLILYÖNTI! Saat 1 juoksun? 
+? (sp-nopat) (kotona onnistut)
+
+
+*/
